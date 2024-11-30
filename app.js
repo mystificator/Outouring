bddd41b695d9d6101908a6f8de2d972b02f01286
mystificator <vipultandon.business@gmail.com>
2024-11-30 18:05:13 +0530

    added dynamic year

--- app.js
+++ app.js
@@ -40,6 +40,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Middleware to pass the current year to all views
+app.use((req, res, next) => {
+    res.locals.currentYear = new Date().getFullYear();
+    next();
+});
+
 app.use(mongoSanitize({
     replaceWith: '_'
 }));
